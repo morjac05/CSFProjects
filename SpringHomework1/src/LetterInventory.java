450bcdf00bfa40d3715426a57e8c43e3b54c8781
morjac05 <jakemorris9875@gmail.com>
2014-04-10 16:18:41 -0700

    Cleaned up, fixed a last-second bug, and removed the main method that was there for debugging purposes. Finished.

--- SpringHomework1/src/LetterInventory.java
+++ SpringHomework1/src/LetterInventory.java
@@ -1,11 +1,11 @@
 public class LetterInventory {
-	char[] characterArray;
-	String matches = "abcdefghijklmnopqrstuvwxyz";
-	int[] count = new int[matches.length()];
+	static String matches = "abcdefghijklmnopqrstuvwxyz";
+	static int[] count = new int[matches.length()];
 	static int sum=0;
-	boolean empty=false;
+	static boolean empty=false;
 	
 	public LetterInventory(String data) {
+		data = data.toLowerCase();
 		for(int i=0; i<data.length(); i++) {
 			int index = matches.indexOf(data.charAt(i));
 			if(index<0) {
@@ -28,14 +28,14 @@ public class LetterInventory {
 		//}
 	//}
 	
-	int get(char letter) {
+	static int get(char letter) {
 		int countindex = matches.indexOf(letter);
 		int lettercount = count[countindex];
 		System.out.println(count[countindex]);
 		return lettercount;
 	}
 	
-	void set(char letter, int value) {
+	static void set(char letter, int value) {
 		int countindex = matches.indexOf(letter);
 		LetterInventory.sum -=count[countindex];
 		count[countindex] = value;
@@ -52,7 +52,7 @@ public class LetterInventory {
 		}
 	}
 	
-	static int size() {
+	static int getSize() {
 		System.out.println(sum);
 		return LetterInventory.sum;
 	}
@@ -80,6 +80,7 @@ public class LetterInventory {
 		return inventory;
 	}
 	
+	@SuppressWarnings("static-access")
 	LetterInventory add(LetterInventory other) {
 		String bothdata = "";
 		LetterInventory combination = new LetterInventory(bothdata);
@@ -90,6 +91,7 @@ public class LetterInventory {
 		return combination;
 	}
 	
+	@SuppressWarnings("static-access")
 	LetterInventory subtract(LetterInventory other) {
 		String bothdata = "";
 		LetterInventory result = new LetterInventory(bothdata);
@@ -104,13 +106,5 @@ public class LetterInventory {
 		}
 		System.out.println(result.toString());
 		return result;
-	}
-	
-	public static void main(String[] args) {
-		String datatest = "AAABCDDDFaabbc";
-		String test2 = "AAABCD";
-		LetterInventory alphabetchecker = new LetterInventory(datatest);
-		LetterInventory inventory2 = new LetterInventory(test2);
-		alphabetchecker.subtract(inventory2);
 	}
 }
